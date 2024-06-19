4fb2f242cf36e796c2fab635a93d4cde2c97890a
Bhavna Garg <bhavnagarg12ja@gmail.com>
2024-06-19 08:40:56 +0530

    navigation bar complete

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,15 @@
+function Hero() {
+  return (
+    <section>
+      <div>
+        <h1>Kick off smarter spending habits</h1>
+        <p>
+          Seize Financial Freedom Anywhere with MoneyMinds. Unlock the
+          Intelligent Path to Managing Your Money!
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -1,50 +1,50 @@
 import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
 import MoneyMinds from "../assets/logos/moneyminds_logo.svg";
+import ButtonLink from "../common/ButtonLink";
 
 function Nav() {
+  const textLinkClasses =
+    "text-gray-500 hover:text-gray-900 active:text-gray-400";
+  const navLinks = [
+    { href: "#features", children: "Features" },
+    { href: "#partners", children: "Partners" },
+    { href: "#reviews", children: "Reviews" },
+  ];
   return (
     <Disclosure as="nav">
       {({ open }) => (
         <>
-          <div className="flex h-[15vh] max-w-7xl items-center justify-between px-8">
+          <div className="flex h-[15vh] max-w-7xl items-center justify-between px-8 lg:px-12 xl:m-auto">
             <div className="flex">
-              <a href="#">
-                <img
-                  src={MoneyMinds}
-                  alt="Money mind logo"
-                  className="h-10 w-auto"
-                ></img>
-              </a>
-              <div className="ml-4 flex hidden items-center space-x-4 sm:flex">
-                <a
-                  className="text-gray-500 hover:text-gray-900 active:text-gray-400"
-                  href="#features"
-                >
-                  Features
-                </a>
-                <a
-                  className="text-gray-500 hover:text-gray-900 active:text-gray-400"
-                  href="#partners"
-                >
-                  Partners
-                </a>
-                <a
-                  className="text-gray-500 hover:text-gray-900 active:text-gray-400"
-                  href="#reviews"
-                >
-                  Reviews
-                </a>
+              <ButtonLink
+                href="#"
+                children={
+                  <img
+                    src={MoneyMinds}
+                    alt="MoneyMinds logo"
+                    className="h-10 w-auto"
+                  />
+                }
+              />
+              <div className="ml-4 hidden items-center space-x-4 sm:ml-6 sm:flex lg:ml-8 lg:space-x-8">
+                {navLinks.map((link, index) => (
+                  <ButtonLink
+                    key={index}
+                    href={link.href}
+                    children={link.children}
+                    className={textLinkClasses}
+                  />
+                ))}
               </div>
             </div>
-            <a
+            <ButtonLink
               href="https://play.google.com"
               target="_blank"
+              children={"Install MoneyMinds"}
               className="hidden rounded-xl bg-gray-800 px-5 py-3 text-white hover:bg-gray-900 active:bg-gray-600 sm:flex"
-            >
-              Install MoneyMinds
-            </a>
-            <Disclosure.Button className="rounded-md p-2 text-gray-500 hover:bg-gray-700 hover:text-white">
+            />
+            <Disclosure.Button className="rounded-md p-2 text-gray-500 hover:bg-gray-700 hover:text-white sm:hidden">
               {open ? (
                 <XMarkIcon className="block h-6 w-6" />
               ) : (
@@ -52,6 +52,18 @@ function Nav() {
               )}
             </Disclosure.Button>
           </div>
+          <Disclosure.Panel>
+            {navLinks.map((link, index) => (
+              <Disclosure.Button
+                className="block"
+                as="a"
+                href="#features"
+                key={index}
+              >
+                {link.children}
+              </Disclosure.Button>
+            ))}
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
